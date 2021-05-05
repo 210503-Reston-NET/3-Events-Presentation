5bb92af3985fd3246fc7e319b3f26a944fb5cb66
Juniper Song <songminseonk@gmail.com>
2021-05-05 14:02:54 -0700

    multiple subscription

--- EventDemo/Program.cs
+++ EventDemo/Program.cs
@@ -6,15 +6,21 @@ public class Program
     {
         ProcessBusinessLogic bl = new ProcessBusinessLogic();
         bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+        bl.ProcessCompleted += bl_EventHandlerTwo;
         bl.StartProcess();
     }
 
     // event handler
     public static void bl_ProcessCompleted(EventData e)
     {      
+        Console.WriteLine("I'm an event handler!");
         Console.WriteLine($"You wrote {e.input}");
         Console.WriteLine("Process Completed!");
     }
+    public static void bl_EventHandlerTwo(EventData e)
+    {
+        Console.WriteLine("I'm another event handler!");
+    } 
 }
 
 public delegate void Notify(EventData e);  // delegate
