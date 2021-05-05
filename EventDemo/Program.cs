2b62b8886a35f6a3c3447c2efaa9cc3143167e5e
kolbym <47865185+kolbym@users.noreply.github.com>
2021-05-05 17:20:42 -0400

    updated

--- EventDemo/Program.cs
+++ EventDemo/Program.cs
@@ -1,46 +1,4 @@
-﻿using System;
-					
-public class Program
-{
-	public static void Main()
-    {
-        ProcessBusinessLogic bl = new ProcessBusinessLogic();
-        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
-        bl.StartProcess();
-    }
 
-    // event handler
-    public static void bl_ProcessCompleted()
-    {
-        Console.WriteLine("Process Completed!");
-    }
-}
-
-public delegate void Notify();  // delegate
-                    
-public class ProcessBusinessLogic
-{
-    public event Notify ProcessCompleted; // event
-
-    public void StartProcess()
-    {
-        Console.WriteLine("Process Started!");
-        
-        
-        // add new code here
-		Console.WriteLine("Press any key to continue");
-        string input = Console.ReadLine();
-        
-        
-        OnProcessCompleted();
-    }
-
-
-    protected virtual void OnProcessCompleted()
-    {
-        ProcessCompleted?.Invoke();
-    }
-}
 using System;
 					
 public class Program
