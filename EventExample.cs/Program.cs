3449d6042fca2c6a32e39339138d38619bd5452b
Bryce Zimbelman <bryce.zimbelman@snhu.edu>
2021-05-05 15:38:42 -0500

    Implemented .Net Event Handler

---
+++ EventExample.cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+    {
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+        bl.StartProcess();
+    }
+
+    // event handler
+    public static void bl_ProcessCompleted(object sender, EventArgs e)
+    {
+        Console.WriteLine("Process Completed!");
+    }
+}
+                   
+public class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted; // event
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        
+        
+        // add new code here
+		    Console.WriteLine("Press any key to continue");
+        Console.ReadLine();
+        
+        
+        OnProcessCompleted();
+    }
+
+
+    protected virtual void OnProcessCompleted(EventArgs e)
+    {
+        ProcessCompleted?.Invoke(this, e);
+    }
+}
